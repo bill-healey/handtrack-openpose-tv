3557db22b4fc7daae5dbb1104ca59e505648c227
Bill Healey <bill@presto.com>
2020-10-28 22:46:17 -0700

    Refactors pose tracking into class, introduces gesture trainer class to capture training images

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+import time
+from pytv import PyTVCursor
+from pose_tracker import PoseTracker
+from gesture_trainer import GestureTrainer
+
+cursor = PyTVCursor()
+cam = cv2.VideoCapture(0)
+pose_tracker = PoseTracker()
+gesture_trainer = GestureTrainer('right_thumbup')
+
+while cv2.waitKey(1) != 27:
+    ret, frame = cam.read()
+    pose = pose_tracker.get_pose_from_frame(frame)
+
+    if pose is None:
+        print("No pose found")
+        time.sleep(1)
+        continue
+
+    cursor.update_world_coordinate(pose['righthand_fingertip_coordinates'])
+    if pose['lefthand_up']:
+        if gesture_trainer:
+            gesture_trainer.capture_training_image(frame, pose['hand_rectangles'][1])
+        else:
+            cursor.click()
+
+cam.release()
+cv2.destroyAllWindows()
