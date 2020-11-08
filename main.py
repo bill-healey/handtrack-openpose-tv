2b516ba52f6c37cee869add83e08d060f0adbfb3
Bill Healey <bill@presto.com>
2020-11-08 12:32:22 -0800

    Introduces hysteresis class to stabilize control.  Uses wrist rather than finger position for cursor movement for better accuracy.  Makes world window slightly smaller to amplify cursor movement.  Saves training images to directories based on confidence.

--- main.py
+++ main.py
@@ -3,28 +3,66 @@ import time
 from pytv import PyTVCursor
 from pose_tracker import PoseTracker
 from gesture_trainer import GestureTrainer
+from hysteresis import Hysteresis
 
 cursor = PyTVCursor()
 cam = cv2.VideoCapture(0)
 pose_tracker = PoseTracker()
 gesture_trainer = GestureTrainer()
+hysteresis = Hysteresis()
+image_collect_only = False
+use_live_camera = True
 
 while cv2.waitKey(1) != 27:
-    ret, frame = cam.read()
-    pose = pose_tracker.predict_pose_from_frame(frame)
-    #pose, frame = pose_tracker.get_pose_and_frame_from_file()
+    if use_live_camera:
+        ret, frame = cam.read()
+        pose = pose_tracker.predict_pose_from_frame(frame)
+    else:
+        pose, frame = pose_tracker.get_pose_and_frame_from_file()
 
     if pose is None:
-        #print("No pose found")
-        time.sleep(1)
+        time.sleep(0.5)
         continue
 
-    cursor.update_world_coordinate(pose['righthand_fingertip_coordinates'])
+    if image_collect_only and pose['righthand_up']:
+        gesture_trainer.capture_training_image(frame, pose['hand_rectangles'], 'rh', 'unrecognized')
+        continue
+
     if pose['righthand_up']:
-        #gesture_trainer.capture_training_image(frame, pose['hand_rectangles'], 'rh', 'unrecognized')
-        gesture_trainer.predict_gesture_from_frame(frame, pose['hand_rectangles'])
-    #if pose['lefthand_up']:
-    #    cursor.click()
+        gesture, confidence = gesture_trainer.predict_gesture_from_frame(frame, pose['hand_rectangles'])
+        cursor.update_world_coordinate(pose['righthand_wrist_coordinates'])
+
+        if confidence > 0.97:
+            hysteresis.update_state(gesture)
+
+        if hysteresis.is_stable('rh_two', secs=0.2, consecutive=2):
+            cursor.keypad_up()
+            hysteresis.reset()
+            print('***Up***')
+        if hysteresis.is_stable('rh_twodown', secs=0.2, consecutive=2):
+            cursor.keypad_down()
+            hysteresis.reset()
+            print('***Down***')
+        if hysteresis.is_stable('rh_twoleft', secs=0.2, consecutive=2):
+            cursor.keypad_left()
+            hysteresis.reset()
+            print('***Left***')
+        if hysteresis.is_stable('rh_tworight', secs=0.2, consecutive=2):
+            cursor.keypad_right()
+            hysteresis.reset()
+            print('***Right***')
+        if hysteresis.is_stable('rh_fingerspread', secs=0.3, consecutive=2):
+            cursor.click()
+            cursor.keypad_ok()
+            hysteresis.reset()
+            print('***Click***')
+        if hysteresis.is_stable('rh_stop', secs=0.3, consecutive=2):
+            cursor.toggle_pause()
+            hysteresis.reset()
+            print('***Pause***')
+        if hysteresis.is_stable('rh_animalhead', secs=0.2, consecutive=2) and pose['lefthand_up']:
+            cursor.keypad_back()
+            print('***Back***')
 
 cam.release()
 cv2.destroyAllWindows()
